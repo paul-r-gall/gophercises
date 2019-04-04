45662e8a02dcaca8f72ac0de8a297c124d46a33a
paul-r-gall <paul.robert.gallagher@gmail.com>
2019-04-04 12:34:43 -0400

    added timer to quiz

--- quiz/quiz1.go
+++ quiz/quiz1.go
@@ -9,6 +9,7 @@ import (
 	"bufio"
 	"regexp"
 	"strings"
+	"time"
 )
 
 func parseQuest(q string) string {
@@ -21,11 +22,51 @@ func parseAns(a string) string {
 	return re.FindString(a)
 }
 
+func awaitAns(ansCha chan string) {
+	var input string
+	fmt.Scanln(&input)
+	ansCha <- input
+	return
+}
+
 func main() {
+	tChannel := make(chan bool, 1)
+	ansChannel := make(chan string, 1)
 	csvFile, _ := os.Open("problems.csv")
 	reader := csv.NewReader(bufio.NewReader(csvFile))
+	
 	correct := 0
 	total := 0
+	fmt.Println("Hit Enter to start the quiz")
+	fmt.Scanln()
+	go func() {
+		time.Sleep(30*time.Second)
+		tChannel <- true
+	}()
+	Loop:
+		for {
+			line, err := reader.Read()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				log.Fatal(err)
+			}
+			total += 1
+			fmt.Println(parseQuest(line[0]))
+			go awaitAns(ansChannel)
+			select {
+			case ans := <-ansChannel:
+				if ans == parseAns(line[1]) {
+					fmt.Println("Correct!")
+					correct+=1
+				} else {
+					fmt.Println("Incorrect.")
+				}
+			case <-tChannel:
+				break Loop
+			}	
+		}
+
 	for {
 		line, err := reader.Read()
 		if err == io.EOF {
@@ -33,16 +74,8 @@ func main() {
 		} else if err != nil {
 			log.Fatal(err)
 		}
+		fmt.Println(line)
 		total += 1
-		fmt.Println(parseQuest(line[0]))
-		var input string
-		fmt.Scanln(&input)
-		if input == parseAns(line[1]) {
-			fmt.Println("Correct!")
-			correct+=1
-		} else {
-			fmt.Println("Incorrect.")
-		}
 	}
 
 	fmt.Printf("You got %d out of %d correct", correct, total)
